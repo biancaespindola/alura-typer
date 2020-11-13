54e4b08c77fb67354921ec531ca989fb6fae5b5a
Bianca Espíndola <32000570+BiancaEspindola@users.noreply.github.com>
2020-11-12 23:59:39 -0300

    gamer over

--- public/js/main.js
+++ public/js/main.js
@@ -1,40 +1,62 @@
-var frase = $(".frase").text();
-var numPalavras = frase.split(" ").length;
-var tamanhoFrase = $("#tamanho-frase");
-// console.log(tamanhoFrase);
+var tempoInicial = $("#tempo-digitacao").text();
 
-tamanhoFrase.text(numPalavras);
+$(document).ready(function() {
+  console.log("pagina carregada");
+  atualizaTamanhoFrase();
+  inicializaContadores();
+  inicializaCronometro();
+  $("#botao-reiniciar").click(reiniciaJogo);
+});
+
+//função que atualiza o tempo que a frase leva para ser digitadae quantas letras tem
+function atualizaTamanhoFrase(){
+  var frase = $(".frase").text();
+  var numPalavras = frase.split(/\S+/).length - 1;
+  var tamanhoFrase = $("#tamanho-frase");
+  console.log(numPalavras);
+  tamanhoFrase.text(numPalavras);
+  
+};
 
 var campo = $(".campo-digitacao");
-// console.log(campo);
-campo.on("input", function () {
-  var conteudo = campo.val();
 
-  var qtdPalavras = conteudo.split(/\S+/).length - 1;
-  $("#contador-palavras").text(qtdPalavras);
+function inicializaContadores(){
+  campo.on("input", function () {
+    var conteudo = campo.val();
+    
+    var qtdPalavras = conteudo.split(/\S+/).length - 1;
+    $("#contador-palavras").text(qtdPalavras);
+    
+    var qtdCaracteres = conteudo.length;
+    $("#contador-caracteres").text(qtdCaracteres);
+  });
+  
+}
 
-  var qtdCaracteres = conteudo.length;
-  $("#contador-caracteres").text(qtdCaracteres);
-});
-
-var tempoRestante = $("#tempo-digitacao").text();
-campo.one("focus", function () {
-  var cronometroID = setInterval(function () {
-    tempoRestante--;
-    // console.log(tempoRestante);
-    $("#tempo-digitacao").text(tempoRestante);
-    if (tempoRestante < 1) {
-      campo.attr("disabled", true);
-      clearInterval(cronometroID);
-    }
-  }, 1000);
-});
+function inicializaCronometro() {
+  var tempoRestante = $("#tempo-digitacao").text();
+  campo.one("focus", function () {
+    var cronometroID = setInterval(function(){
+      tempoRestante--;
+      // console.log(tempoRestante);
+      $("#tempo-digitacao").text(tempoRestante);
+      if (tempoRestante < 1) {
+        campo.attr("disabled", true);
+        clearInterval(cronometroID);
+      }
+    }, 1000);
+  });
+}
 
 // $("#botao-reiniciar").on("click", function(){
 //   console.log("Botao clicado");
 // });
 
-$("#botao-reiniciar").click(function(){
+function reiniciaJogo(){
   campo.attr("disabled", false);
   campo.val("");
-});
+  $("#contador-palavras").text("0");
+  $("#contador-caracteres").text("0");
+  $("#tempo-digitacao").text(tempoInicial);
+}
+
