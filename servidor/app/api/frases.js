e0ef6f0c172185fc5f33bb3a3a18eb73f3c16a43
Bianca Espíndola <32000570+BiancaEspindola@users.noreply.github.com>
2020-11-20 16:23:15 -0300

    atualizando as frases da api

--- servidor/app/api/frases.js
+++ servidor/app/api/frases.js
@@ -1,16 +1,16 @@
 var api = {};
 
 var frases = [
-	{_id: 0, texto:'Alura, Cursos online de tecnologia que reinventam sua carreira.', tempo: 15 },
-	{_id: 1, texto:'Debuggers não consertam erros, apenas os exibem em slow motion.',tempo: 8 },
-	{_id: 2, texto:'Caelum, Ensino e Inovação.', tempo: 5 },
-	{_id: 3, texto:'Existem duas tarefas difíceis na Ciência da Computação: invalidação de cache e nomear as coisas.', tempo: 15 },
-	{_id: 4, texto:'Ciência da computação é tão sobre computadores quanto astronomia é sobre telescópios.', tempo: 15 },
-	{_id: 5, texto:'Na minha máquina funciona.', tempo: 5 },
-	{_id: 6, texto:'Hardware é o que você chuta, software é o que você xinga.', tempo: 12 },
-	{_id: 7, texto:'Software em funcionamento mais que documentação abrangente.', tempo: 10 },
-	{_id: 8, texto:'Iterar é humano, recursão é divino.', tempo: 7},
-	{_id: 9, texto:'Existem três jeitos de desenvolver software. O jeito certo, o jeito errado e o meu jeito, que é igual o jeito errado só que mais rápido.', tempo: 20},
+	{_id: 0, texto:'Ohana significa família, família significa que ninguém é deixado para trás ou esquecido.', tempo: 20 },
+	{_id: 1, texto:'Eles dizem que se você sonhar com uma coisa mais de uma vez essa coisa se torna realidade.',tempo: 22 },
+	{_id: 2, texto:'Olhe além do que você vê.', tempo: 5 },
+	{_id: 3, texto:'Por mais que o vento sopre, uma montanha nunca irá se ajoelhar diante dele.', tempo: 17 },
+	{_id: 4, texto:'Deixe seu coração lhe guiar. Ele sussurra, então preste atenção.', tempo: 13 },
+	{_id: 5, texto:'Prenda-se àquilo que o faz diferente.', tempo: 7 },
+	{_id: 6, texto:'A flor que nasce em meio a diversidade é a mais rara e bonita de todas.', tempo: 15 },
+	{_id: 7, texto:'A jornada de mil quilômetros começa com o primeiro passo.', tempo: 10 },
+	{_id: 8, texto:'O passado pode doer, mas da maneira que eu vejo, você pode fugir dele ou aprender com ele', tempo: 20},
+	{_id: 9, texto:'Todos os seus sonhos podem se tornar realidade se você tiver a coragem para persegui-los', tempo: 20},
 
 	];
 
